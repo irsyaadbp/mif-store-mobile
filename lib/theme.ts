f776c1944dd3b7629975189d5731f9a324027792
Irsyaad Budi <irsyaadbp01@gmail.com>
2026-02-08 16:33:17 +0700

    fix: color variable dark mode

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,5 +1,5 @@
 import { DarkTheme, DefaultTheme, type Theme } from '@react-navigation/native';
- 
+
 export const THEME = {
   light: {
     background: 'hsl(0 0% 100%)',
@@ -28,33 +28,33 @@ export const THEME = {
     chart5: 'hsl(27 87% 67%)',
   },
   dark: {
-    background: 'hsl(0 0% 3.9%)',
-    foreground: 'hsl(0 0% 98%)',
-    card: 'hsl(0 0% 3.9%)',
-    cardForeground: 'hsl(0 0% 98%)',
-    popover: 'hsl(0 0% 3.9%)',
-    popoverForeground: 'hsl(0 0% 98%)',
-    primary: 'hsl(0 0% 98%)',
-    primaryForeground: 'hsl(0 0% 9%)',
-    secondary: 'hsl(0 0% 14.9%)',
-    secondaryForeground: 'hsl(0 0% 98%)',
-    muted: 'hsl(0 0% 14.9%)',
-    mutedForeground: 'hsl(0 0% 63.9%)',
-    accent: 'hsl(0 0% 14.9%)',
-    accentForeground: 'hsl(0 0% 98%)',
-    destructive: 'hsl(0 70.9% 59.4%)',
-    border: 'hsl(0 0% 14.9%)',
-    input: 'hsl(0 0% 14.9%)',
-    ring: 'hsl(300 0% 45%)',
+    background: 'hsl(0 0% 100%)',
+    foreground: 'hsl(0 0% 0%)',
+    card: 'hsl(0 0% 100%)',
+    cardForeground: 'hsl(0 0% 0%)',
+    popover: 'hsl(0 0% 100%)',
+    popoverForeground: 'hsl(0 0% 0%)',
+    primary: 'hsl(26 100% 50%)',
+    primaryForeground: 'hsl(0 0% 100%)',
+    secondary: 'hsl(0 0% 0%)',
+    secondaryForeground: 'hsl(0 0% 100%)',
+    muted: 'hsl(210 20% 98%)',
+    mutedForeground: 'hsl(0 0% 33%)',
+    accent: 'hsl(210 20% 98%)',
+    accentForeground: 'hsl(0 0% 0%)',
+    destructive: 'hsl(0 84.2% 60.2%)',
+    border: 'hsl(0 0% 90%)',
+    input: 'hsl(210 20% 98%)',
+    ring: 'hsl(26 100% 50%)',
     radius: '0.625rem',
-    chart1: 'hsl(220 70% 50%)',
-    chart2: 'hsl(160 60% 45%)',
-    chart3: 'hsl(30 80% 55%)',
-    chart4: 'hsl(280 65% 60%)',
-    chart5: 'hsl(340 75% 55%)',
+    chart1: 'hsl(12 76% 61%)',
+    chart2: 'hsl(173 58% 39%)',
+    chart3: 'hsl(197 37% 24%)',
+    chart4: 'hsl(43 74% 66%)',
+    chart5: 'hsl(27 87% 67%)',
   },
 };
- 
+
 export const NAV_THEME: Record<'light' | 'dark', Theme> = {
   light: {
     ...DefaultTheme,
